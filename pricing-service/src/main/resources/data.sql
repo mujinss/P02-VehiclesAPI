3c8ea6b5bc304deaa086791d3fe69dac395c1838
ss <helensunlife@gmail.com>
2019-09-19 12:02:05 -0700

    added test for pricing-service api

---
+++ pricing-service/src/main/resources/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO price (vehicle_Id, currency, price) VALUES(1, 'USD', '42000');
+INSERT INTO price (vehicle_Id, currency, price) VALUES(2, 'RMB', '160000');
+INSERT INTO price (vehicle_Id, currency, price) VALUES(3, 'GPD', '98076');
--- pricing-service/src/test/java/com/udacity/pricing/PricingServiceApplicationTests.java
+++ pricing-service/src/test/java/com/udacity/pricing/PricingServiceApplicationTests.java
@@ -1,16 +1,47 @@
 package com.udacity.pricing;
 
+import com.udacity.pricing.entity.Price;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.json.JacksonTester;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.CoreMatchers.equalTo;
+
+
+import java.math.BigDecimal;
+import java.net.URI;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(webEnvironment= SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class PricingServiceApplicationTests {
+	@Autowired
+	private TestRestTemplate restTemplate;
+
+	@LocalServerPort
+	private int port;
 
 	@Test
 	public void contextLoads() {
+	}
+
+	@Test
+	public void findPrice() {
+		ResponseEntity<Price> response = this.restTemplate.getForEntity("http://localhost:8082/prices/1", Price.class);
+		assertThat(response.getStatusCode(), equalTo(HttpStatus.OK));
 	}
 
 }
